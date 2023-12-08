ac9cd3970a5bcdb4e765973aaddab76aabf83401
igoradm <igorpetrov42ru@gmail.com>
2023-12-08 23:19:44 +0300

    Не получается пока что использовать атрибуты валидации и span, возможно нужны js скрипты?

--- Task_6/Controllers/Home.cs
+++ Task_6/Controllers/Home.cs
@@ -8,24 +8,13 @@ public class HomeController : Controller
     [HttpGet]
     public IActionResult Index() => View();
     [HttpPost]
-    public IActionResult Index(string login, int pin_code ) 
+    public IActionResult Index(string login, int pin_code, string email) 
     {
-        if (ModelState.IsValid) {
-            ViewBag.Message = $"{login} - {pin_code}";
+        if (ModelState.IsValid) 
+        {
+            ViewBag.Message = $"{login} - {pin_code} - {email}";
             return View();
         }
-        var strErrors = new StringBuilder();
-        foreach (var item in ModelState) 
-        {
-            strErrors.Append(item.Key + " " + item.Value + "\n");
-        }
-        foreach (var item in ModelState) 
-        {
-            foreach (var error in item.Value.Errors) 
-            {
-                strErrors.Append($"{item.Key} {error.ErrorMessage}\n");
-            }
-        }
-        return Unauthorized(strErrors.ToString());
-    } 
+        return View();
+    }
 }
--- Task_6/Models/User.cs
+++ Task_6/Models/User.cs
@@ -2,8 +2,13 @@ using System.ComponentModel.DataAnnotations;
 namespace AppModels;
 public class User 
 {
-    [Required]
+    [Required (ErrorMessage = "Логин не указан")]
+    [RegularExpression (@"\s", ErrorMessage = "В логине не должно быть пробелов")]
+    [StringLength (50,MinimumLength = 3, ErrorMessage = "Длина логина больше 3 и меньше 50")]
     public string login;
-    [Required]
+    [Required (ErrorMessage = "пин-код не указан")]
+    [StringLength (5,MinimumLength = 4, ErrorMessage = "Пин-код имеет четырехзначное значение")]
     public int pin_code;
+    [Required]
+    public string email;
 }
