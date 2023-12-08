314eaa2f78fbd7d08455d4abb8e6161c97fd7d03
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-12-08 20:59:23 +0300

    Add files via upload

--- Task_6/Controllers/Home.cs
+++ Task_6/Controllers/Home.cs
@@ -1,6 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using AppModels;
 namespace MvcApp;
-class HomeController : Controller 
+public class HomeController : Controller 
 {
+    [HttpGet]
     public IActionResult Index() => View();
+    [HttpPost]
+    public IActionResult Index(string login, int pin_code ) 
+    {
+        if (ModelState.IsValid) {
+            ViewBag.Message = $"{login} - {pin_code}";
+            return View();
+        }
+        else return Unauthorized("Name or pin no valid");
+    } 
+
 }
