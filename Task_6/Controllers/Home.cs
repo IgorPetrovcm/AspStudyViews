b4f8228a25327ef3d6173397d727f69f2e91f2cb
igoradm <igorpetrov42ru@gmail.com>
2023-12-08 22:50:01 +0300

    выводим ошибки ввода используя ModelState

--- Task_6/Controllers/Home.cs
+++ Task_6/Controllers/Home.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using AppModels;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 namespace MvcApp;
 public class HomeController : Controller 
 {
@@ -12,7 +14,18 @@ public class HomeController : Controller
             ViewBag.Message = $"{login} - {pin_code}";
             return View();
         }
-        else return Unauthorized("Name or pin no valid");
+        var strErrors = new StringBuilder();
+        foreach (var item in ModelState) 
+        {
+            strErrors.Append(item.Key + " " + item.Value + "\n");
+        }
+        foreach (var item in ModelState) 
+        {
+            foreach (var error in item.Value.Errors) 
+            {
+                strErrors.Append($"{item.Key} {error.ErrorMessage}\n");
+            }
+        }
+        return Unauthorized(strErrors.ToString());
     } 
-
 }
