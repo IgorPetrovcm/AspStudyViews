1c03314dde86f7b4431b5177f5263e7ea5806a2f
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-12-11 18:03:00 +0300

    Add files via upload

---
+++ Task_7/Controllers/Home.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApp;
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Task_7/Models/Person.cs
@@ -0,0 +1,5 @@
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+namespace MvcModels 
+{
+    public record class Person(string name, int age);
+}
